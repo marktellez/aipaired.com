ebdd598474f3f71c1886e3cda424a64993f04f32
Alex Tseitlin <alextseitlin@Alexs-MBP.lan>
2023-04-21 14:33:27 +0300

    footer fix

--- src/features/layout/footer/index.jsx
+++ src/features/layout/footer/index.jsx
@@ -6,15 +6,15 @@ export function Footer({}) {
   return (
     <footer className="">
       <Container>
-        <div className="sm:flex items-start gap-1 w-full">
-          <div className="sm:flex flex-col w-full gap-4 mt-24">
+        <div className="sm:flex items-start w-full text-sm">
+          <div className="sm:flex flex-col w-full mt-24">
             <div className="px-6 text-white flex items-center gap-2 justify-end">
               <EnvelopeOpenIcon className="w-5 " />
               <div className="font-thin">hello@aipaired.com</div>
             </div>
 
             <div className="w-full bg-purple-500 rounded-full h-1" />
-            <div className="sm:px-6 sm:flex gap-10">
+            <div className=" sm:flex gap-10 py-3">
               <div className="flex-grow">
                 &copy; 2022-{new Date().getFullYear()} AI Paired, llc
               </div>
